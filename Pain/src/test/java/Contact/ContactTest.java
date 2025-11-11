174a249742bd5a15fc8ca12feb72cef21b168c93
prith <prithvinpr1001@gmail.com>
2025-11-11 13:21:51 +0530

    New script added in contact

--- Pain/src/test/java/Contact/ContactTest.java
+++ Pain/src/test/java/Contact/ContactTest.java
@@ -6,6 +6,17 @@ public class ContactTest {
 @Test(groups = "SmokeTest")
 public void createContactTest() {
 	System.out.println("ContactTest is generated");
+	String URL = System.getProperty("url","http://amazon.in");
+	String BROWSER = System.getProperty("browser","Chrome");
+	String USERNAME = System.getProperty("username","Admin");
+	String PASSWORD = System.getProperty("password","Admin");
+	
+	
+	
+	System.out.println(URL);
+	System.out.println(BROWSER);
+	System.out.println(USERNAME);
+	System.out.println(PASSWORD);
 }
 @Test(groups = "RegressionTest")
 public void modifyContactTest() {
